5c785432c92db507b75c054283b61bb5d0594c33
Sabatino Provenza <sabatinoprovenza98@outlook.it>
2026-01-20 14:00:07 +0100

    Initial commit

---
+++ src/main/java/sabatinoprovenza/Application.java
@@ -0,0 +1,15 @@
+package sabatinoprovenza;
+
+
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class Application {
+
+    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("BE-S3-L2pu");
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+    }
+}
